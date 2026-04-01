3d946824ef4f0dfd6f0fe8baca37090ecee53505
Leila <leiila.hort@gmail.com>
2026-04-01 13:08:07 -0300

    refatora BaseSeleniumPage para evitar falhas no workflow

--- backend/todo-api/src/test/java/br/com/infnet/todo_api/Selenium/core/BaseSeleniumPage.java
+++ backend/todo-api/src/test/java/br/com/infnet/todo_api/Selenium/core/BaseSeleniumPage.java
@@ -10,31 +10,31 @@ import java.time.Duration;
 import java.util.List;
 
 public class BaseSeleniumPage {
+
   protected WebDriver driver;
   protected WebDriverWait wait;
+
   public BaseSeleniumPage(WebDriver driver) {
     this.driver = driver;
-    this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    this.wait = new WebDriverWait(driver, Duration.ofSeconds(25));
   }
 
-  public void esperarElementoVisivel(By locator) {
-    wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+  protected WebElement esperarElementoVisivel(By locator) {
+    return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
   }
 
-  public void esperarElementoClicavel(By locator) {
-    wait.until(ExpectedConditions.elementToBeClickable(locator));
+  protected WebElement esperarElementoClicavel(By locator) {
+    return wait.until(ExpectedConditions.elementToBeClickable(locator));
   }
 
   protected void clicar(By locator) {
-    esperarElementoClicavel(locator);
-    driver.findElement(locator).click();
+    WebElement elemento = esperarElementoClicavel(locator);
+    elemento.click();
   }
 
   protected void preencherCampo(By locator, String valor) {
-    esperarElementoVisivel(locator);
-    WebElement campo = driver.findElement(locator);
+    WebElement campo = esperarElementoVisivel(locator);
     campo.clear();
     campo.sendKeys(valor);
   }
-
 }
