35d4c2cfd2361c2b66355581fc75f1a49aac6517
Leila <leiila.hort@gmail.com>
2026-04-01 12:53:04 -0300

    corrige execução do Selenium no CI com Chrome headless

--- backend/todo-api/src/test/java/br/com/infnet/todo_api/Selenium/core/BaseSeleniumTest.java
+++ backend/todo-api/src/test/java/br/com/infnet/todo_api/Selenium/core/BaseSeleniumTest.java
@@ -1,14 +1,14 @@
 package br.com.infnet.todo_api.Selenium.core;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class BaseSeleniumTest {
+
   protected WebDriver driver;
 
   @BeforeAll
@@ -18,9 +18,18 @@ public class BaseSeleniumTest {
 
   @BeforeEach
   void abrirNavegador(){
-    driver = new ChromeDriver();
-    driver.get("http://localhost:5173/");
-    driver.manage().window().maximize();
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--disable-gpu");
+    options.addArguments("--window-size=1920,1080");
+
+    driver = new ChromeDriver(options);
+
+    //driver.get("http://localhost:5173/");
+    driver.get("http://localhost:3000/");
   }
 
   @AfterEach
